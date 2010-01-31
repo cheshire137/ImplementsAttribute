01625c23a9d5af1f54de2cc3ab1170bb951e4237
Sarah Vessels <cheshire137@gmail.com>
2010-01-30 19:42:35 -0500

    Adding project for testing ImplementsAttribute and ImplementsChecker.  Updating gitignore file to ignore executables and assemblies.

---
+++ TestImplementsAttribute/IMyInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestImplementsAttribute
+{
+    interface IMyInterface
+    {
+        int Foo();
+        string FooWithString(string value);
+    }
+}
---
+++ TestImplementsAttribute/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using ImplementsAttribute;
+
+namespace TestImplementsAttribute
+{
+    public class MyClass : IMyInterface
+    {
+        [Implements(typeof(IMyInterface))]
+        public int Foo()
+        {
+            return 3;
+        }
+
+        public string FooWithString(string value)
+        {
+            return value;
+        }
+    }
+}
